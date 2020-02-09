9be016eeabe80120a9bfaef063344802cbc1ace6
Be-libra <50490129+Be-libra@users.noreply.github.com>
2020-02-10 00:14:22 +0530

    correcting positioning

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Akki-solutions/precache-manifest.33bc3e55627aab69249c6639f4127cbc.js"
+  "/Akki-solutions/precache-manifest.69878a3a95752f3ffac9c981121e4400.js"
 );
 
 self.addEventListener('message', (event) => {
